c45ef85ae4f00582b85c3a959abe3e38a7208864
RCAbney <ryan@rcabney.com>
2022-05-21 18:24:36 -0400

    update conditional render on svg divider width

--- src/components/QuoteCard/QuoteCard.js
+++ src/components/QuoteCard/QuoteCard.js
@@ -10,8 +10,18 @@ const QuoteCard = () => {
     e.preventDefault();
     getQuote(url, setQuote, setFetching);
   };
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 769);
   const [quote, setQuote] = useState({});
   const [fetching, setFetching] = useState(true);
+
+  const updateWidth = () => {
+    setIsMobile(window.innerWidth < 769);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  });
 
   useEffect(() => {
     const url = `https://api.adviceslip.com/advice`;
@@ -32,15 +42,27 @@ const QuoteCard = () => {
         <div className={styles.quoteWrap}>
           <h1 className={styles.advice}>Advice #{quote?.slip?.id}</h1>
           <p className={styles.quoteText}>"{quote?.slip?.advice}"</p>
-          <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fillRule="evenodd">
-              <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
-              <g transform="translate(138)" fill="#CEE3E9">
-                <rect width="6" height="16" rx="3" />
-                <rect x="14" width="6" height="16" rx="3" />
+          {isMobile ? (
+            <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fillRule="evenodd">
+                <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+                <g transform="translate(138)" fill="#CEE3E9">
+                  <rect width="6" height="16" rx="3" />
+                  <rect x="14" width="6" height="16" rx="3" />
+                </g>
               </g>
-            </g>
-          </svg>
+            </svg>
+          ) : (
+            <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fillRule="evenodd">
+                <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
+                <g transform="translate(212)" fill="#CEE3E9">
+                  <rect width="6" height="16" rx="3" />
+                  <rect x="14" width="6" height="16" rx="3" />
+                </g>
+              </g>
+            </svg>
+          )}
         </div>
       )}
       <Button handleClick={handleClick} />
